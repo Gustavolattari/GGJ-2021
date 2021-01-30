a70511511b09c522775a185b43b6ef532e30dfbc
Gustavo Lattari <gustavolattari@gmail.com>
2021-01-29 19:37:23 -0500

    Added gary, added movement, added grab collider.

--- GGJ2021/Assets/Scripts/Player/PlayerController.cs
+++ GGJ2021/Assets/Scripts/Player/PlayerController.cs
@@ -20,18 +20,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
     }
 
     void HandleInput()
     {
         if (Input.GetKey(KeyCode.A))
         {
-
+            Move(Vector3.left);
         }
         if (Input.GetKey(KeyCode.D))
         {
-
+            Move(Vector3.right);
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Move(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Move(Vector3.back);
+        }
+    }
+
+    void Move(Vector3 dir)
+    {
+        transform.Translate(dir.normalized * Time.deltaTime * moveSpeed, Space.World);
+        //transform.LookAt(Camera.main.transform.position, Vector3.up);
     }
 }
