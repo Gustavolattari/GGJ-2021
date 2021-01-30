86a7c8b7288dc4f1b2c71a3ab138dfca295825d6
Dang Nguyen <Dang_nguyen_95@yahoo.com>
2021-01-30 15:11:20 -0500

    Fixing current menu

--- GGJ2021/Assets/Dang/DangScript/DN_MainMenuMannager.cs
+++ GGJ2021/Assets/Dang/DangScript/DN_MainMenuMannager.cs
@@ -6,10 +6,19 @@ public class DN_MainMenuMannager : MonoBehaviour
 {
     public GameObject StartUI;
     private Animator StartAnimator;
+    public GameObject HTPUI;
+    private Animator HTPAnimator;
+    public GameObject CreditUI;
+    private Animator CreditAnimator;
+    public GameObject QuitUI;
+    private Animator QuitAnimator;
     // Start is called before the first frame update
     void Start()
     {
         StartAnimator = StartUI.GetComponent<Animator>();
+        HTPAnimator = HTPUI.GetComponent<Animator>();
+        CreditAnimator = CreditUI.GetComponent<Animator>();
+        QuitAnimator = QuitUI.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,5 +33,29 @@ public class DN_MainMenuMannager : MonoBehaviour
     public void StopHover()
     {
         StartAnimator.SetBool("Hover", false);
+    }
+    public void StartHTPHover()
+    {
+        HTPAnimator.SetBool("Hover", true);
+    }
+    public void StopHTPHover()
+    {
+        HTPAnimator.SetBool("Hover", false);
+    }
+    public void StartCreditHover()
+    {
+        CreditAnimator.SetBool("Hover", true);
+    }
+    public void StopCreditHover()
+    {
+        CreditAnimator.SetBool("Hover", false);
+    }
+    public void StartQuitHover()
+    {
+        QuitAnimator.SetBool("Hover", true);
+    }
+    public void StopQuitHover()
+    {
+        QuitAnimator.SetBool("Hover", false);
     }
 }
